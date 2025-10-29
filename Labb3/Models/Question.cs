a8bba7c71204bf5b94f0b0584efc6e5cc4c73296
Blaze_Draconian <Dangus360@gmail.com>
2025-10-29 15:42:08 +0100

    Add project files.

---
+++ Labb3/Models/Question.cs
@@ -0,0 +1,16 @@
+﻿namespace Labb3.Models
+{
+
+    internal class Question
+    {
+        public Question(string query, string correctAnswer, string IncorrectAnswer1, string IncorrectAnswer2, string IncorrectAnswer3)
+        {
+            Query = query;
+            CorrectAnswer = correctAnswer;
+            IncorrectAnswers = [IncorrectAnswer1, IncorrectAnswer2, IncorrectAnswer3];
+        }
+        public string Query { get; set; }
+        public string CorrectAnswer { get; set; }
+        public string[] IncorrectAnswers { get; set; }
+    }
+}
---
+++ Labb3/ViewModels/QuestionPackViewModel.cs
@@ -0,0 +1,66 @@
+﻿using Labb3.Models;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace Labb3.ViewModels
+{
+
+    internal class QuestionPackViewModel : ViewModelBase
+    {
+        private readonly QuestionPack _model;
+        public QuestionPackViewModel(QuestionPack model)
+        {
+            _model = model;
+            Questions = new ObservableCollection<Question>(_model.Questions);
+            Questions.CollectionChanged += Questions_CollectionChanged;
+        }
+
+        private void Questions_CollectionChanged(object? sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            if (e.Action == NotifyCollectionChangedAction.Add && e.NewItems != null)
+            foreach(Question q in e.NewItems)_model.Questions.Add(q);
+
+                if (e.Action == NotifyCollectionChangedAction.Remove && e.OldItems != null)
+                foreach (Question q in e.OldItems) _model.Questions.Add(q);
+
+            if (e.Action == NotifyCollectionChangedAction.Replace && e.OldItems != null && e.NewItems != null)
+                _model.Questions[e.OldStartingIndex] = (Question)e.NewItems[0]!;
+
+            if (e.Action == NotifyCollectionChangedAction.Reset)
+                _model.Questions.Clear();
+            
+        }
+
+        public string Name
+        {
+            get => _model.Name;
+            set {_model.Name = value; 
+                
+                RaisePropertyChanged(); }
+           
+        }
+
+        public Difficulty difficulty
+        {
+            get => _model.Difficulty;
+            set { _model.Difficulty = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public int TimeLimitInSeconds
+        {
+            get => _model.TimeLimitInSeconds;
+            set
+            {
+                _model.TimeLimitInSeconds = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public ObservableCollection<Question> Questions { get; set; }
+    }
+}
