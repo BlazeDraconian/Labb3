30392c50d6b97bf2693868ad4e9c18a258767b4e
Blaze_Draconian <Dangus360@gmail.com>
2025-10-30 12:26:18 +0100

    Updated application

--- Labb3/MainWindow.xaml.cs
+++ Labb3/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using Labb3.Models;
+using Labb3.ViewModels;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -14,9 +16,20 @@ namespace Labb3
     
     public partial class MainWindow : Window
     {
+        private int count = 1;
         public MainWindow()
         {
             InitializeComponent();
+
+            var pack = new QuestionPack("MyQuestionPack");
+            DataContext = new QuestionPackViewModel(pack);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            QuestionPackViewModel viewModel = (DataContext as QuestionPackViewModel);
+            viewModel.Name = "New name";
+            viewModel.Questions.Add(new Question($"Fråga {count++}", "2", "3", "1", "4"));
         }
     }
 }
