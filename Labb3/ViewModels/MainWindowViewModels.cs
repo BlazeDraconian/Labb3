5d737d8dd73f8c3fe18c01d74bde04d7688f53e7
Blaze_Draconian <Dangus360@gmail.com>
2025-10-31 11:57:41 +0100

    Updated Program

---
+++ Labb3/ViewModels/MainWindowViewModels.cs
@@ -0,0 +1,40 @@
+﻿using Labb3.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb3.ViewModels
+{
+    class MainWindowViewModel: ViewModelBase
+    {
+        public ObservableCollection<QuestionPackViewModel> Packs { get; } = new();
+
+		private QuestionPackViewModel _activePack;
+
+		public QuestionPackViewModel ActivePack
+		{
+			get =>_activePack;
+			set { 
+				_activePack = value;
+				RaisePropertyChanged();
+				PlayerViewModel.RaisePropertyChanged(nameof(PlayerViewModel.ActivePack));
+				}
+		}
+        public PlayerViewModel? PlayerViewModel { get; set; }
+		public ConfigurationViewModel ConfigurationViewModel { get;}
+
+        public MainWindowViewModel()
+		{
+			PlayerViewModel = new PlayerViewModel(this);
+			ConfigurationViewModel = new ConfigurationViewModel(this);
+
+			new PlayerViewModel(this);
+            var pack = new QuestionPack("MyQuestionPack");
+            ActivePack = new QuestionPackViewModel(pack);
+            ActivePack.Questions.Add(new Question($"Vad heter Sveriges huvudstad?", "Stockholm", "Göteborg", " Malmö", "Helsingborg"));
+		}
+	}
+}
---
+++ Labb3/ViewModels/PlayerViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Labb3.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb3.ViewModels
+{
+    class PlayerViewModel : ViewModelBase
+    {
+        private readonly MainWindowViewModel? _mainWindowViewModel;
+
+        public DelegateCommand SetPackNameCommand { get; }
+        public QuestionPackViewModel ActivePack { get => _mainWindowViewModel.ActivePack; }
+
+        public PlayerViewModel(MainWindowViewModel? mainWindowViewModel)
+        {
+            this._mainWindowViewModel = mainWindowViewModel;
+            SetPackNameCommand = new DelegateCommand(SetpackName);
+        }
+
+        private void SetpackName(object? obj)
+        {
+            ActivePack.Name = "New name";
+        }
+    }
+}
